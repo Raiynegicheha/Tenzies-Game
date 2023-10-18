5758de54b81bb78ede41d3b672d83cfb79aa72a3
Ruth Gicheha <raiynegicheha@gmail.com>
2023-10-18 23:05:02 +0300

    Create App.js

---
+++ tenzies-game/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Die from './Die';
+
+
+function App() {
+  return (
+    <main className='dice-container'> 
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+      <Die value = "1"/>
+    </main>
+  );
+}
+
+export default App;
