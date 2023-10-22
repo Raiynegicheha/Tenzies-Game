a98a026c04ea67b670d70723869f2a9356de38e1
Ruth Gicheha <raiynegicheha@gmail.com>
2023-10-22 23:52:51 +0300

    Update Die.js

--- tenzies-game/src/Die.js
+++ tenzies-game/src/Die.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./App.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faDiceOne,
+    faDiceTwo,
+    faDiceThree,
+    faDiceFour,
+    faDiceFive,
+    faDiceSix,
+  } from "@fortawesome/free-solid-svg-icons";
+
+
 
 
 export default function Die(props){
+
+   
+    const getIcon = (value) =>{
+        switch(value){
+            case 1: 
+                return <FontAwesomeIcon icon={faDiceOne} />;
+            case 2: 
+                return <FontAwesomeIcon icon={faDiceTwo} />;
+            case 3: 
+                return <FontAwesomeIcon icon={faDiceThree} />;
+            case 4: 
+                return <FontAwesomeIcon icon={faDiceFour} />;
+            case 5: 
+                return <FontAwesomeIcon icon={faDiceFive} />;
+            case 6: 
+                return <FontAwesomeIcon icon={faDiceSix} />;
+        }
+    }
+    
+
     return (
         <div className={props.isHeld? "die-face hold" : "die-face"} onClick={props.holdDice}>
-            <h2 className="tenzie">{props.value}</h2>
+
+            <div className="tenzie">{getIcon(props.value)}</div>
+            {/* <h2 className="tenzie">{props.value}</h2> */}
 
         </div>
             
